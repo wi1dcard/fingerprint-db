bbea6f0db9ef73abb0299e2df408e8e5bbaffdb4
wi1dcard <wi1dcard.cn@gmail.com>
2024-02-18 21:28:41 +0800

    Finish main.go.

--- go/main.go
+++ go/main.go
@@ -4,11 +4,13 @@ import (
 	"crypto/tls"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
-	url := "https://localhost:8443/"
+	url := "https://localhost:8443/json"
 	method := "GET"
 
 	tr := &http.Transport{TLSClientConfig: &tls.Config{
@@ -19,21 +21,24 @@ func main() {
 	req, err := http.NewRequest(method, url, nil)
 
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 
 	res, err := client.Do(req)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
 	defer res.Body.Close()
 
 	body, err := ioutil.ReadAll(res.Body)
 	if err != nil {
-		fmt.Println(err)
-		return
+		log.Fatal(err)
 	}
+
+	err = os.WriteFile("fingerprint.json", body, 0644)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	fmt.Println(string(body))
 }
