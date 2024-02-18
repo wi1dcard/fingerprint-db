390b87ffa5ca44c1b3f30234a74a3843744d4485
wi1dcard <wi1dcard.cn@gmail.com>
2024-02-18 17:55:10 +0800

    Add certs and test.

--- go/main.go
+++ go/main.go
@@ -1,16 +1,21 @@
 package main
 
 import (
+	"crypto/tls"
 	"fmt"
 	"io/ioutil"
 	"net/http"
 )
 
 func main() {
-	url := "https://httpbin.org/anything"
+	url := "https://localhost:8443/"
 	method := "GET"
 
-	client := &http.Client{}
+	tr := &http.Transport{TLSClientConfig: &tls.Config{
+		InsecureSkipVerify: true,
+	}}
+
+	client := &http.Client{Transport: tr}
 	req, err := http.NewRequest(method, url, nil)
 
 	if err != nil {
