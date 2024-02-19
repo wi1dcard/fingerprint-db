75e1d112b3fe4383983004503f40421841cc6099
wi1dcard <wi1dcard.cn@gmail.com>
2024-02-19 18:57:05 +0800

    Add superagent user-agent.

--- node/superagent.js
+++ node/superagent.js
@@ -6,6 +6,7 @@ superagent
   .get('https://localhost:8443/json')
   .disableTLSCerts()
   .http2()
+  .set('User-Agent', 'superagent')
   .end((err, res) => {
     fs.appendFileSync('fingerprint.json', res.text);
     process.stdout.write(res.text)
